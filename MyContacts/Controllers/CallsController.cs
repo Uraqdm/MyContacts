ed017396fd039da4e24cd9d6055351eabf478e12
urauymanov <urauymanov@yandex.ru>
2021-10-19 21:32:45 +0700

    Adding conrtoller vith view

---
+++ MyContacts/Controllers/CallsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyContacts.DatabaseLayer;
+using System.Threading.Tasks;
+
+namespace MyContacts.Controllers
+{
+    public class CallsController : Controller
+    {
+        private readonly Context _context;
+
+        public CallsController(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() => View(await _context.Calls.ToListAsync());
+    }
+}
