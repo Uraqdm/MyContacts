a980d4f80a3919c87ff71b3657e02c85bc8b180f
urauymanov <urauymanov@yandex.ru>
2021-10-19 21:32:03 +0700

    adding conferention controller

---
+++ MyContacts/Controllers/ConferentionsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyContacts.DatabaseLayer;
+using MyContacts.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MyContacts.Controllers
+{
+    public class ConferentionsController : Controller
+    {
+        private readonly Context _context;
+
+        public ConferentionsController(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() => View(await _context.Conferentions.ToListAsync());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Conferention conferention)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Conferentions.AddAsync(conferention);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(conferention);
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var conferention = await _context.Conferentions.FindAsync(id);
+
+            if(conferention == null)
+            {
+                return NotFound();
+            }
+
+            return View(conferention);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Conferention conferention)
+        {
+            if (id != conferention.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Conferentions.Update(conferention);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(conferention);
+        }
+    }
+}
