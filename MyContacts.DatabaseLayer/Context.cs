a35967e2f12af94ff9bf636cb63d6c8d9fe25302
urauymanov <urauymanov@yandex.ru>
2021-10-19 13:34:28 +0700

    Adding db context and EF core for ms sql.

---
+++ MyContacts.DatabaseLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyContacts.Models;
+
+namespace MyContacts.DatabaseLayer
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Call> Calls { get; set; }
+
+        public DbSet<Conferention> Conferentions { get; set; }
+
+        public DbSet<Phone> PhoneNumbers { get; set; }
+    }
+}
