d87f7c4b66cd1e2c03eea5f8bc878e533d84d78e
urauymanov <urauymanov@yandex.ru>
2021-10-19 16:38:21 +0700

    adding phone validation

--- MyContacts.Models/Phone.cs
+++ MyContacts.Models/Phone.cs
@@ -9,6 +9,8 @@ namespace MyContacts.Models
         public Guid Id { get; set; }
 
         [Required]
+        [Display(Name = "Номер телефона")]
+        [Phone]
         public string PhoneNumber { get; set; }
 
     }
