b3d249d8da2923a56302da5cb6021adc41dc165a
urauymanov <urauymanov@yandex.ru>
2021-10-19 15:34:02 +0700

    changing routes

--- MyContacts/Startup.cs
+++ MyContacts/Startup.cs
@@ -44,7 +44,9 @@ namespace MyContacts
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
             serviceProvider.GetService<Context>()?.Database.EnsureCreated();
