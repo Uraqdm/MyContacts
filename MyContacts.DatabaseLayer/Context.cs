eba53d787b00dc58331c0cd206ca9ed5f839e5ec
urauymanov <urauymanov@yandex.ru>
2021-10-20 19:47:50 +0700

    removing contact adding

--- MyContacts.DatabaseLayer/Context.cs
+++ MyContacts.DatabaseLayer/Context.cs
@@ -31,18 +31,6 @@ namespace MyContacts.DatabaseLayer
                 Id = new Guid("6d8dae9f-de21-4dda-a952-dcc0103351d1"),
                 PhoneNum = "89054122233"
             });
-
-            //Creating contact
-            modelBuilder.Entity<Contact>().HasData(new Contact
-            {
-                Id = new Guid("53503136-b159-4538-95b4-68cecace6828"),
-                Name = "Ivan",
-                MiddleName = "Ivanovich",
-                LastName = "Ivanov",
-                PhoneNumberId = new Guid("e7aaa2b2-207d-4626-a74d-a8af8d41b239")
-
-            });
-
         }
     }
 }
