c65412e7ac7a73eef6c465f96f4dc6398b664e83
urauymanov <urauymanov@yandex.ru>
2021-10-21 09:54:42 +0700

    adding auth by number method

--- MyContacts/Controllers/HomeController.cs
+++ MyContacts/Controllers/HomeController.cs
@@ -1,9 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyContacts.DatabaseLayer;
+using MyContacts.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyContacts.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Context _context;
+
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Enter(PhoneNumberViewModel phoneNumber)
+        {
+            if (ModelState.IsValid)
+            {
+                var phone = await _context.PhoneNumbers.Where(p => p.PhoneNum == phoneNumber.PhoneNum).FirstOrDefaultAsync();
+
+                if(phone == null)
+                {
+                    return View(phoneNumber);
+                }
+
+                return RedirectToAction(controllerName: "Contacts", actionName: "Index");
+            }
+
+            return View(phoneNumber);
+        }
     }
 }
